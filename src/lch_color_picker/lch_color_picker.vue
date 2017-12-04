dfe64205bcf89410429dd11511ba0996ffd0e566
senritsuki <senritsuki@gmail.com>
2017-12-05 02:29:03 +0900

    レイアウト修正

--- src/lch_color_picker/lch_color_picker.vue
+++ src/lch_color_picker/lch_color_picker.vue
@@ -1,19 +1,19 @@
 <template lang="pug">
 #lch_color_picker
   el-row
-    el-col(:xs="24", :sm="24", :md="12")
+    el-col(:xs="24", :sm="12", :md="12")
       .box
-        svg(viewBox="-112.5 -112.5 225 225", :class="{black: bg_black}")
+        svg(width="450", height="450", viewBox="-112.5 -112.5 225 225", :class="{black: bg_black}")
           g
             circle.clickable(v-for="c in colors_h", :cx="c.x", :cy="c.y", :r="c.r", :fill="c.c.rgbhex", stroke="none", @click="select_color(c.c.lch)", @mouseover="hover_color(c.c.lch)", @mouseout="hover_color(null)")
             circle(v-for="c in colors_h_mark", :cx="c.x", :cy="c.y", :r="c.r", :fill="c.c.rgbhex", stroke="none")
           g(transform="translate(-55, 55), scale(1.10, -0.55)")
             circle.clickable(v-for="c in colors_lc", :cx="c.x", :cy="c.y", :r="c.r", :fill="c.c.rgbhex", stroke="none", @click="select_color(c.c.lch)", @mouseover="hover_color(c.c.lch)", @mouseout="hover_color(null)")
             circle(v-for="c in colors_lc_mark", :cx="c.x", :cy="c.y", :r="c.r", :fill="c.c.rgbhex", stroke="none")
-    el-col(:xs="24", :sm="16", :md="12")
+    el-col(:xs="24", :sm="12", :md="12")
       .box
-        div(:style="{backgroundColor: rgbhex, height: '10px'}")
-    el-col(:xs="24", :sm="16", :md="12")
+        div(:style="{backgroundColor: rgbhex, height: '12px'}")
+    el-col(:xs="24", :sm="12", :md="12")
       .box
         el-row
           el-col.slider-label(:span="6") Lightness
@@ -24,7 +24,7 @@
         el-row
           el-col.slider-label(:span="6") Hue
           el-col(:span="18"): el-slider(v-model="lch_h", :max="360", :step="5", show-input)
-    el-col(:xs="24", :sm="24", :md="12")
+    el-col(:xs="24", :sm="12", :md="12")
       .box
         el-row
           el-col.label(:span="14") L*C*h
@@ -59,7 +59,7 @@
           el-col.value(:span="3") {{rgb255_f[0]}},
           el-col.value(:span="3") {{rgb255_f[1]}},
           el-col.value(:span="4") {{rgb255_f[2]}}
-    el-col(:xs="24", :sm="24", :md="12")
+    el-col(:xs="24", :sm="12", :md="12")
       .box
         el-switch(v-model="bg_black", inactive-text="Background White", active-text="Background Black")
 </template>
@@ -69,16 +69,16 @@
 
 <style>
 #lch_color_picker {
+  color: #333;
+  font-size: 90%;
+  font-family: 'Verdana', sans-serif;
   box-sizing: border-box;
-  font-family: 'Times Roman', 'Times New Roman';
 }
 #lch_color_picker .box {
-  width: 480px;
+  /*width: 480px;*/
   padding: 15px;
 }
 #lch_color_picker svg {
-  width: 448px;
-  height: 448px;
   background-color: #fff;
 }
 #lch_color_picker svg.black {
@@ -88,18 +88,20 @@
   cursor: pointer;
 }
 #lch_color_picker .slider-label {
-  font-weight: bold;
+  font-weight: normal;
   line-height: 32px;
 }
 #lch_color_picker .label {
-  font-weight: bold;
+  font-size: 90%;
+  font-weight: normal;
   line-height: 30px;
   border-bottom: 1px solid #e6ebf5;
 }
 #lch_color_picker .value {
   text-align: left;
   color: #409eff;
-  /*font-family: monospace;*/
+  font-size: 90%;
+  font-weight: normal;
   line-height: 30px;
   border-bottom: 1px solid #e6ebf5;
 }
